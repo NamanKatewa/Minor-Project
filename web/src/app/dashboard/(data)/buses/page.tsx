f0bb7ffbc1572fde6fa32537cb9827cb8748a68e
Naman Katewa <namankatewa2004@gmail.com>
2026-02-07 12:41:25 +0530

    chore: Use Client in SSR

--- web/src/app/dashboard/(data)/buses/page.tsx
+++ web/src/app/dashboard/(data)/buses/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import type { ColumnDef, Row } from "@tanstack/react-table";
 import { Trash2 } from "lucide-react";
