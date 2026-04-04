2b42e8d6f7ac97c44291f845d94a21a26bbca048
Naman Katewa <namankatewa2004@gmail.com>
2026-04-04 12:40:52 +0530

    fix: Remove dashboard redirect

--- web/next.config.js
+++ web/next.config.js
@@ -5,16 +5,6 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {
-	async redirects() {
-		return [
-			{
-				source: "/dashboard",
-				destination: "/dashboard/stops",
-				permanent: true,
-			},
-		];
-	},
-};
+const config = {};
 
 export default config;
